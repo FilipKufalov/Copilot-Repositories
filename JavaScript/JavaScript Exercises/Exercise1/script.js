abebfb5e933f2b2b5b788c8511f583d6ff342f98
FilipKufalov <filip.kufalov@hotmail.com>
2022-04-16 05:02:23 +0200

    Second Experience

--- JavaScript/JavaScript Exercises/Exercise1/script.js
+++ JavaScript/JavaScript Exercises/Exercise1/script.js
@@ -15,3 +15,20 @@ function toggleImage() {
         image2.style.display = "block";
     }
 }
+
+
+//functio for multiplying two numbers
+function multiply() {
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var result = num1 * num2;
+    document.getElementById("result").textContent = result;
+}
+
+//function for divide two numbers
+function divide() {
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var result = num1 / num2;
+    document.getElementById("result").textContent = result;
+}
---
+++ JavaScript/Maths/script.js
@@ -0,0 +1,55 @@
+// Console Logs are for testing purposes
+
+// Function for plus
+function plus(a,b){
+    return a+b;
+}
+console.log("Calculation for 5+5=" + plus(5,5));
+
+// Function for minus
+function minus(a,b){
+    return a-b;
+}
+console.log("Calculation for 5-5=" + minus(5,5));
+
+// Function for multiply
+function multiply(a,b){
+    return a*b;
+}
+console.log("Calculation for 5*5=" + multiply(5,5));
+
+// Function for divide
+function divide(a,b){
+    return a/b;
+}
+console.log("Calculation for 5/5=" + divide(5,5));
+
+// Function for modulus
+function modulus(a,b){
+    return a%b;
+}
+console.log("Calculation for 5%5=" + modulus(10,2));
+
+// Function for exponent
+function exponent(a,b){
+    return Math.pow(a,b);
+}
+console.log("Calculation for 5^5=" + exponent(5,5));
+
+// Function for square root
+function squareRoot(a){
+    return Math.sqrt(a);
+}
+console.log("Calculation for square root of 5=" + squareRoot(5));
+
+// Function for square
+function square(a){
+    return Math.pow(a,2);
+}
+console.log("Calculation for square of 4=" + square(4));
+
+// Function for cube
+function cube(a){
+    return Math.pow(a,3);
+}
+console.log("Calculation for cube of 5=" + cube(5));
